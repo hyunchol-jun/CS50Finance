40fdb8e5c39c8e4c4f53d8799f5225486bc7042e
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-03-25 11:50:57 -0400

    Create SQLAlchemy model

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from tempfile import mkdtemp
 import sqlite3
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required, apology, lookup, usd
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -28,6 +30,25 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure sqlalchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    hash = db.Column(db.String(120), unique=True, nullable=False)
+    cash = db.Column(db.Float, nullable=False, default=10000.00)
+    records = db.relationship('Stock', backref='holders', lazy=True)
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(20), nullable=False)
+    shares = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False, default=0.00)
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    
 # Create sqlite3 database connection 
 connection = sqlite3.connect("finance.db", check_same_thread=False)
 connection.isolation_level = None
